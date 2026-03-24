611e52397615966e6ca0965065180b58b3045ed5
LaurentMbuyu <laurent.mbuyu@takeda.com>
2026-03-24 21:00:35 +0200

    [New]: Added button as a component

---
+++ src/app/core/ui/button/button.component.ts
@@ -0,0 +1,12 @@
+import { Component, Input } from '@angular/core';
+
+@Component({
+  selector: 'app-button',
+  templateUrl: './button.component.html',
+  styleUrl: './button.component.scss',
+  standalone: true
+})
+export class ButtonComponent {
+  @Input({ required: true }) label!: string;
+  @Input() type: 'button' | 'submit' | 'reset' = 'button';
+}
