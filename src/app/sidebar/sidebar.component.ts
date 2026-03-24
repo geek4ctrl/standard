76b91b05495a061c0eb05d9160738a1d09346ca8
LaurentMbuyu <laurent.mbuyu@takeda.com>
2026-03-24 19:59:38 +0200

    [New]: Added material icons and an array for sidebar items

--- src/app/sidebar/sidebar.component.ts
+++ src/app/sidebar/sidebar.component.ts
@@ -1,8 +1,28 @@
 import { Component } from '@angular/core';
+import { SidebarItem } from './sidebar-item.model';
 
 @Component({
   selector: 'app-sidebar',
   templateUrl: './sidebar.component.html',
   styleUrl: './sidebar.component.scss'
 })
-export class SidebarComponent {}
+export class SidebarComponent {
+  readonly items: SidebarItem[] = [
+    {
+      id: 1,
+      label: 'Home',
+      icon: 'home',
+      isActive: true
+    },
+    {
+      id: 2,
+      label: 'Profiles',
+      icon: 'badge'
+    },
+    {
+      id: 3,
+      label: 'Third',
+      icon: 'description'
+    }
+  ];
+}
