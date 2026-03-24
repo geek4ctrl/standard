fc81dee912566316cf6bd870e103ad5c51791d11
LaurentMbuyu <laurent.mbuyu@takeda.com>
2026-03-24 19:39:28 +0200

    [New]: Added navigation bar and sidebar components

--- src/app/app.ts
+++ src/app/app.ts
@@ -1,9 +1,11 @@
 import { Component, signal } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
+import { SidebarComponent } from './sidebar/sidebar.component';
+import { NavigationBarComponent } from './navigation-bar/navigation-bar.component';
 
 @Component({
   selector: 'app-root',
-  imports: [RouterOutlet],
+  imports: [RouterOutlet, SidebarComponent, NavigationBarComponent],
   templateUrl: './app.html',
   styleUrl: './app.scss'
 })
